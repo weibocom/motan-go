dd13ba5af4775d2e408b8c4ce5deb9e38d2cd1dd
luominggang <minggang@staff.weibo.com>
2018-10-11 18:05:52 +0800

    add group auto detect

---
+++ core/ping_test.go
@@ -0,0 +1,19 @@
+package core
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestNewPinger(t *testing.T) {
+	pinger, err := NewPinger("127.0.0.1", 5, 5*time.Second, 56)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+	pinger.Ping()
+	fmt.Printf("total count: %d\n", len(pinger.Rtts))
+	for _, rtt := range pinger.Rtts {
+		fmt.Println(rtt)
+	}
+}
