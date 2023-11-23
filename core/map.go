7dcecdb32154931297fb9679f403d9285b0e4840
arraykeys <arraykeys@gmail.com>
2023-11-23 14:39:59 +0800

    Optimise StringMap Range

--- core/map.go
+++ core/map.go
@@ -48,17 +48,8 @@ func (m *StringMap) LoadOrEmpty(key string) string {
 // If f returns false, range stops the iteration
 func (m *StringMap) Range(f func(k, v string) bool) {
 	m.mu.RLock()
-	keys := make([]string, 0, len(m.innerMap))
-	for k := range m.innerMap {
-		keys = append(keys, k)
-	}
-	m.mu.RUnlock()
-
-	for _, k := range keys {
-		v, ok := m.Load(k)
-		if !ok {
-			continue
-		}
+	defer m.mu.RUnlock()
+	for k, v := range m.innerMap {
 		if !f(k, v) {
 			break
 		}
