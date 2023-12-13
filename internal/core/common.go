07791c1b633e1c5eb71b957ada44c381cba86e23
arraykeys <arraykeys@gmail.com>
2023-12-13 16:35:19 +0800

    try ant pool

--- internal/core/common.go
+++ internal/core/common.go
@@ -2,7 +2,7 @@ package core
 
 import "github.com/panjf2000/ants/v2"
 
-var processV2Pool, _ = ants.NewPool(20 * 10000)
+var processV2Pool, _ = ants.NewPool(60 * 10000)
 
 func GetProcessV2Pool() *ants.Pool {
 	return processV2Pool
